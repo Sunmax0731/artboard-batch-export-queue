d699de07e1cbefea3657c2c71a6167c9d0f8fee1
Sunmax0731 <gkk.jhon@gmail.com>
2026-05-08 05:35:26 +0900

    Implement strict pre-release baseline

---
+++ plugins/artboard-batch-export-queue/src/main.jsx
@@ -0,0 +1,9 @@
+/* アートボード一括書き出しキュー */
+(function () {
+  var profile = {
+    name: "アートボード一括書き出しキュー",
+    host: "Illustrator",
+    repository: "artboard-batch-export-queue"
+  };
+  $.writeln(profile.name + " loaded for " + profile.host);
+}());
